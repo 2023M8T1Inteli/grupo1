462f51fc619fc9322c54acb6f5dbed4372096594
GabrielNhoncanse <gabriel.nhoncanse@sou.inteli.edu.br>
2023-11-07 17:53:27 -0300

    Versão inicial árvore binária

--- src/compilador/analisador_lexico/tree.py
+++ src/compilador/analisador_lexico/tree.py
@@ -1,12 +1,46 @@
 class Tree:
     def __init__(self):
-        self.nodeList = []
+        self.root = None
+        self.depth = 0
 
     def addNode(self, node):
-        self.nodeList.append(node)
+        if self.root == None:
+            self.root = node
+            self.depth += 1
+        else:
+            self.add(self.root, node)
+
+    def add(self, currentNode, newNode):
+        if currentNode.left == None:
+            currentNode.left = newNode
+            self.depth += 1
+        elif currentNode.right == None:
+            currentNode.right = newNode
+        else:
+            self.add(currentNode, newNode)
 
     def __repr__(self):
-        return "\n".join(map(str, self.nodeList))
+        return self.printTree(self.root)
+
+    def printTree(self, node):
+        if node != None:
+            print(" " + str(node))
+            if node.left != None and node.right != None:
+                print("/" + " " + "\\")
+                print(self.printTree(node.left) + " " + self.printTree(node.right))
+            elif node.left != None:
+                print("/" + " ")
+                print(self.printTree(node.left))
+            elif node.right != None:
+                print("\\")
+                print(self.printTree(node.right))
+        return ""
+        # if node is not None:
+        #     output = "  " * level + str(node) + "\n"
+        #     output += self.printTree(node.left, level + 1)
+        #     output += self.printTree(node.right, level + 1)
+        #     return output
+        # return ""
 
 
 class Node:
@@ -16,7 +50,7 @@ class Node:
         self.right = None
 
     def __repr__(self) -> str:
-        return f"Node(value={self.value}, left={self.left}, right={self.right})"
+        return self.value
 
 
 tree = Tree()
