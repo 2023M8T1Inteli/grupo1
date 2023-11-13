c0fa61a6d0e4a19041c2516b6ca194c8d116bc21
mateusalm <mateus.almeida@sou.inteli.edu.br>
2023-11-12 21:58:38 -0300

    comentários

--- src/compilador/analisador_lexico/app.py
+++ src/compilador/analisador_lexico/app.py
@@ -5,20 +5,20 @@ from Compiler import compile, read_code
 app = Flask(__name__)
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST']) # define uma rota para fazer upload do arquivo .txt com o código gerado
 def upload_file():
     uploaded_file = request.files['file'] 
 
     if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
+        uploaded_file.save(uploaded_file.filename) # salva o arquivo de código gerado
 
-        source_code = read_code('code.txt')
+        source_code = read_code('code.txt') # leitura do arquivo
 
-        compile(source_code)
+        compile(source_code) # compila o código gerado, passando pelos analisadores léxico e sintárico
 
 
 
     return 'Nenhum arquivo enviado.'
 
 if __name__ == '__main__':
-    app.run() 
+    app.run() # inicialização do servidor
--- src/frontend/scripts/geracaoCodigo.js
+++ src/frontend/scripts/geracaoCodigo.js
@@ -1,42 +1,43 @@
 import { Conditional } from './Conditional.js'
 
-const editor = document.getElementById('editor')
+const editor = document.getElementById('editor') // editor de código
 
-const generator = document.getElementById('gerador')
+const generator = document.getElementById('gerador') // botão para gerar código
 
-generator.addEventListener('click', generateCode)
+generator.addEventListener('click', generateCode) // função de gerar código adicionada ao botão
 
 function generateCode() {
+  // início do programa
   let codeText = 'programa "atividade": '
 
   codeText += 'inicio '
 
-  let codeElements = editor.childNodes
+  let codeElements = editor.childNodes // cda elemento do código é um elemento filho (dentro do editor)
 
+  // passa por cada elemento e adiciona seu conteúdo a string que salva o código
   for (let i = 0; i < codeElements.length; i++) {
-    console.log('elemento ' + i + ' : ' + codeElements[i].id)
-
     if (codeElements[i].id == 'if-statement') {
+      // verifica se o elemento é um bloco condicional
       let block = new Conditional(codeElements[i])
       codeText += block.getBlock() + ' '
     } else {
-      codeText += codeElements[i].id + ' '
+      codeText += codeElements[i].id + ' ' // o id do elemento guarda seu símbolo correspondente na linguagem QAL
     }
   }
 
-  codeText += ' fim.'
+  codeText += ' fim.' // adicioona o fim do programa
 
-  console.log(codeText)
-
-  sendCode('http://127.0.0.1:5000/', codeText)
+  sendCode('http://127.0.0.1:5000/', codeText) // envio do arquivo para o servidor
 }
 
 function sendCode(backendURL, code) {
+  // cria um arquivo .txt com o argumento 'code' como conteúdo
   const blob = new Blob([code], { type: 'text/plain' })
 
   const formData = new FormData()
   formData.append('file', blob, 'code.txt')
 
+  // realiza o upload do arquivo gerado para o servidor
   fetch(backendURL + '/upload', {
     method: 'POST',
     body: formData
@@ -45,7 +46,6 @@ function sendCode(backendURL, code) {
       console.log('Arquivo enviado com sucesso.')
     })
     .catch(error => {
-      // Lida com erros
       console.error('Ocorreu um erro ao enviar o arquivo:', error)
     })
 }
