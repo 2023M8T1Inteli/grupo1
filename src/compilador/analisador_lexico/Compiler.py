0b9268fb76ff50c248e57647a3a2ddfadf37d5c3
mateusalm <mateus.almeida@sou.inteli.edu.br>
2023-11-12 21:46:59 -0300

    refatoração

--- src/compilador/analisador_lexico/Compiler.py
+++ src/compilador/analisador_lexico/Compiler.py
@@ -32,18 +32,4 @@ class Compiler:
         # Lê o código fonte fornecido
         source_code = read_code("codigo.txt")
 
-        # Cria uma instância do analisador léxico e gera os tokens do código
-        tokens = Lexer.lexer(source_code, token_patterns)
-        for token in tokens:
-            print(token)
-
-        # Cria uma instância do analisador sintático e faz a análise com base nos tokens
-        syntax = Syntatic(tokens)
-        syntax.analyze()
-
-        # Cria uma árvore sintática com os tokens
-        tree = Tree()
-        for token in tokens:
-            node = Node(token)
-            tree.add_node(node)
-            tree.print_tree(tree.root)
+        compile(source_code)
--- src/compilador/analisador_lexico/app.py
+++ src/compilador/analisador_lexico/app.py
@@ -1,23 +1,8 @@
 from flask import Flask, request
-import subprocess
-import sys
-import os
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__),  '..'))
-
-sys.path.append(path)
-
-from Lexer import Lexer
-from Syntactic import Syntatic
-from token_patterns import token_patterns
+from Compiler import compile, read_code
 
 app = Flask(__name__)
-
-def read_code(file): 
-    code = ""
-    with open(file, "r", encoding="utf-8") as f:
-        code = f.read()
-    return code
 
 
 @app.route('/upload', methods=['POST'])
@@ -29,14 +14,7 @@ def upload_file():
 
         source_code = read_code('code.txt')
 
-        tokens = Lexer.lexer(source_code, token_patterns)
-        for token in tokens:
-            print(token)
-
-        syntax = Syntatic(tokens)
-        syntax.analyze()
-
-        print('ok')
+        compile(source_code)
 
 
 
