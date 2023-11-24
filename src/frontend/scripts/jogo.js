d5a80f7a41a025158421403a62c96f2d61e7c094
mateusalm <mateus.almeida@sou.inteli.edu.br>
2023-11-24 11:08:45 -0300

    tela de jogo para apresentação sprint3

--- src/frontend/scripts/jogo.js
+++ src/frontend/scripts/jogo.js
@@ -5,22 +5,36 @@ var options = document.getElementsByClassName('tapete-cor')
 
 var playAgain = document.getElementsByClassName('play-again')
 
+const overlay = document.getElementById('overlay')
+
+const exitBtn = document.getElementById('sair')
+
 for (let i = 0; i < options.length; i++) {
   let option = options[i]
 
   if (option.getAttribute('data-cor') == 'red') {
-    option.onclick()
+    option.addEventListener('click', function () {
+      rightAnswerModal.style.display = 'block'
+      overlay.style.display = 'block'
+    })
+  } else {
+    option.addEventListener('click', function () {
+      wrongAnswerModal.style.display = 'block'
+      overlay.style.display = 'block'
+    })
   }
 }
 
-playAgain.forEach(btn => {
-  btn.onclick = function () {
-    modal.style.display = 'none'
-  }
+for (let i = 0; i < playAgain.length; i++) {
+  let btn = playAgain[i]
+
+  btn.addEventListener('click', function () {
+    rightAnswerModal.style.display = 'none'
+    wrongAnswerModal.style.display = 'none'
+    overlay.style.display = 'none'
+  })
+}
+
+exitBtn.addEventListener('click', function () {
+  window.location.href = './codigo.html'
 })
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none'
-  }
-}
