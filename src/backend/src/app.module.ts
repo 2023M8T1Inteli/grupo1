28609aa9d86d896e9a813ad61a780411b6bf4f1a
IaagoTavares <99209029+IaagoTavares@users.noreply.github.com>
2023-12-08 11:09:01 -0300

    including updates

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -6,11 +6,13 @@ import { PacienteController } from './paciente/paciente.controller';
 import { PrismaService } from './prisma.service';
 import { PacienteService } from './paciente/paciente.service';
 import { UpImageController } from './up-image/up-image.controller';
+import { UpModule } from './up-image/up-image.module';
+import { upService } from './up-image/up-image.service';
 
 
 @Module({
-  imports: [PacienteModule, AtividadeModule, TerapeutaModule],
+  imports: [PacienteModule, AtividadeModule, TerapeutaModule, UpModule],
   controllers: [PacienteController, UpImageController],
-  providers: [PacienteService, PrismaService],
+  providers: [PacienteService, PrismaService, upService],
 })
 export class AppModule {}
--- src/backend/src/up-image/up-image.controller.ts
+++ src/backend/src/up-image/up-image.controller.ts
@@ -1,10 +1,42 @@
-import { Controller,Get } from '@nestjs/common';
+// import { Controller,Post, Body } from '@nestjs/common';
 
+
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Res } from "@nestjs/common";
+import { upImage } from "./up-image.model";
+import { upService } from "./up-image.service";
+import { Request, Response } from "express";
 
 @Controller('up-image')
 export class UpImageController {
+  constructor(private readonly upService: upService) {}
+
   @Get()
-  getHello(): string {
-    return 'Olá, mundo!';
+  async getAllBook(@Req() request:Request, @Res() response:Response ):Promise<any>{
+       const result =  await this.upService.getAllBook()
+       return response.status(200).json({
+            status: "Ok!",
+            message: "Successfully fetch data!",
+            result: result 
+       })
+  }
+
+  @Post()
+  async postBook(@Body() postData: upImage):Promise<upImage>{
+       return this.upService.createBook(postData)
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id:number):Promise<upImage | null>{
+       return this.upService.getBook(id)
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id:number):Promise<upImage>{
+       return this.upService.deleteBook(id)
+  }
+
+  @Put(':id')
+  async updateBook(@Param('id') id: number,@Body() data: upImage): Promise<upImage> {
+    return this.upService.updateBook(id,data);
   }
 }
